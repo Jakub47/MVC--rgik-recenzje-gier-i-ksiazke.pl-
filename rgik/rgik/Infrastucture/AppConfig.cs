ab66f9caeea33b5f29f863596491a0df3ca12a75
Jakub47 <jakub7447@gmail.com>
2018-02-18 23:05:46 +0100

    Url Problem Fixed

---
+++ rgik/rgik/Infrastucture/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace rgik.Infrastucture
+{
+    /// <summary>
+    /// Konektor do Web.config
+    /// </summary>
+    public class AppConfig
+    {
+        private static string _obrazkiFolderWzgledny = ConfigurationManager.AppSettings["ObrazkiFolder"];
+
+        public static string ObrazkiFolderWzgledny
+        {
+            get
+            {
+                return _obrazkiFolderWzgledny;
+            }  
+        }
+    }
+}
---
+++ rgik/rgik/Infrastucture/UrlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rgik.Infrastucture
+{
+    public static class UrlHelpers
+    {
+        public static string ObrazkiSciezka(this UrlHelper helper, string nazwaObrazka )
+        {
+            var ObrazkiKategoriFolder = AppConfig.ObrazkiFolderWzgledny;
+            var sciezka = Path.Combine(ObrazkiKategoriFolder, nazwaObrazka);
+            var sciezkaBezwzledna = helper.Content(sciezka);
+
+            return sciezkaBezwzledna;
+        }
+    }
+}
