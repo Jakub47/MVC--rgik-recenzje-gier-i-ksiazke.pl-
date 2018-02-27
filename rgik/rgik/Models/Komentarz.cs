f33164da45845d29dab03d9a502a1351e2413ff3
Jakub47 <jakub7447@gmail.com>
2018-02-27 09:38:55 +0100

    v2

---
+++ rgik/rgik/Models/Komentarz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rgik.Models
+{
+    public class Komentarz
+    {
+        public int KomentarzId { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
